5f5c9e5620b242725e02187122fe3ac1cd4b3801
Chloe McDermott <chloe.mcdermott@capita.co.uk>
2021-05-04 14:20:32 +0100

    added unit test for view model

--- data/view_model.py
+++ data/view_model.py
@@ -10,8 +10,29 @@ class ViewModel:
             if item.status == "To Do":
                 output.append(item)
 
+        return output
+
+    def doing_items(self):
+        output = []
+
+        for item in self.items:
+            if item.status == "Doing":
+                output.append(item)
+
+        return output
+
+    def done_items(self):
+        output = []
+
+        for item in self.items:
+            if item.status == "Done":
+                output.append(item)
+
+        return output
+
     @property 
     def items(self):
         return self._items
 
+    
 
--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -1,5 +1,6 @@
 from data.view_model import ViewModel 
 from data.todo_item import Item
+import pytest
 
 def test_view_model_show_todo_items():
     items = [
@@ -17,5 +18,35 @@ def test_view_model_show_todo_items():
 
     assert todo_item.status == "To Do"
 
+def test_view_model_show_doing_items():
+    items = [
+        Item(1, "New Todo", "To Do"),
+        Item(2, "In Progress Todo", "Doing"),
+        Item(3, "Completed", "Done"),
+    ]
+    view_model = ViewModel(items)
 
-    #Time: 24:23 
+    todo_items = view_model.doing_items()
+
+    assert len(todo_items) == 1
+
+    todo_item = todo_items[0]
+
+    assert todo_item.status == "Doing"
+
+def test_view_model_show_done_items():
+    items = [
+        Item(1, "New Todo", "To Do"),
+        Item(2, "In Progress Todo", "Doing"),
+        Item(3, "Completed", "Done"),
+    ]
+    view_model = ViewModel(items)
+
+    todo_items = view_model.done_items()
+
+    assert len(todo_items) == 1
+
+    todo_item = todo_items[0]
+
+    assert todo_item.status == "Done"
+
